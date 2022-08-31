81fe392c21413c87e433ff3398d22f40215ce03b
IlyasMukatdisov <mukatdisovilyas@gmail.com>
2022-08-31 22:58:04 +0500

    created Auth Exception classes

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,46 @@
+//login exceptions
+
+
+class UserNotFoundAuthException implements Exception{
+
+}
+
+class WrongPasswordAuthException implements Exception{
+  
+}
+
+class UserDisabledAuthException implements Exception{
+  
+}
+
+class InvalidEmailAuthExcveption implements Exception{
+  
+}
+
+// Registration exceptions
+
+class EmailAlreadyInUseAuthExcemption implements Exception{
+
+}
+
+class InvalidEmailAuthException implements Exception{
+  
+}
+
+class OperationNotAllowedAAuthException implements Exception{
+  
+}
+
+class WeakPasswordAuthException implements Exception{
+  
+}
+
+// Generic Exceptions
+
+class GenericAuthException implements Exception{
+
+}
+
+class UserNotLoggedInAuthException implements Exception{
+
+}
