484cf7d4a5bd73f4df3c36399497ec3ff5f01c8a
IlyasMukatdisov <mukatdisovilyas@gmail.com>
2022-09-12 20:40:51 +0500

    mics changes

--- lib/pages/utils/show_snackbar.dart
+++ lib/pages/utils/show_snackbar.dart
@@ -18,7 +18,7 @@ Future<void> showErrorDialog(BuildContext context, String message) {
     context: context,
     builder: (context) {
       return AlertDialog(
-        title: const Text('An error occured'),
+        title: const Text('An error occurred'),
         content: Text(message),
         actions: [
           TextButton(
